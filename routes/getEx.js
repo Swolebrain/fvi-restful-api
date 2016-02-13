2b3a1683eef88db83226bc194264fc9bb9d1bc19
Swolebrain <vic574m@gmail.com>
2016-02-13 17:31:24 -0500

    route by difficulty
    Added code to get a set of exercises by difficulty

--- routes/getEx.js
+++ routes/getEx.js
@@ -1,26 +1,33 @@
 module.exports = function(exercises){
   return function(req, res){
-      if (req.params.arg1==="all"){
-          var retVal = [];
-          res.setHeader('Content-Type', 'application/json');
-          for (var i = 0; i < exercises.length; i++){
-              retVal.push({
-                  "shortName" : exercises[i].shortName,
-                  "url" : exercises[i].url
-              });
-          }
-          res.end(JSON.stringify(  retVal  ) );
-          return;
-      }
-      var index = 0;
-      for (var i = 0; i < exercises.length; i++){
-          if (exercises[i].url === req.params.arg1){
-              index = i;
-              break;
-          }
-      } 
-      var retVal = exercises[i]; 	
+    if (req.params.arg1==="all"){
+        var retVal = [];
+        res.setHeader('Content-Type', 'application/json');
+        for (var i = 0; i < exercises.length; i++){
+            retVal.push({
+                "shortName" : exercises[i].shortName,
+                "url" : exercises[i].url
+            });
+        }
+        res.end(JSON.stringify(  retVal  ) );
+        return;
+    } //end all case
+    else if(req.params.arg1.indexOf("$diff") === 0){
+      var diff = req.params.arg1.split("-")[1];
+      var retVal = exercises.filter( e => e.difficulty===diff );
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(retVal));
+      return;
+    }
+    var index = 0;
+    for (var i = 0; i < exercises.length; i++){
+        if (exercises[i].url === req.params.arg1){
+            index = i;
+            break;
+        }
+    } 
+    var retVal = exercises[i]; 	
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(retVal));
   }
 }
