a427a670f01078ff65dd1043efd2732a0413d557
Swolebrain <thecodingteacher@gmail.com>
2016-07-02 13:47:30 -0400

    added fake form posting endpoint for final exam

--- routes/velocifyForms.js
+++ routes/velocifyForms.js
@@ -23,14 +23,20 @@ module.exports =  {
 }
 
 function validateForm(frm){
-		if ( frm.fname.length < 2 || frm.lname.length < 2)
-			return "Please include your first name";
+  if (!frm.fname){
+    frm.first_name = frm.fname;
+    frm.last_name = frm.lname;
+    frm.phone = frm.phone1;
+  }
+	if ( frm.fname.length < 2 || frm.lname.length < 2)
+		return "Please include your first name";
 
-		if (!frm.email.match(/^[A-z-_.]{4,50}@[A-z-_.]+\.[A-z]{2,4}$/) )
-			return "Please enter an email with the structure address@domain.com (or .net, .co, etc)";
+	if (!frm.email.match(/^[A-z-_.]{4,50}@[A-z-_.]+\.[A-z]{2,4}$/) )
+		return "Please enter an email with the structure address@domain.com (or .net, .co, etc)";
 
-		if (!frm.phone.match(/^[( ]*[0-9]{3}[) -]*[0-9]{3}[ -]*[0-9]{4}$/) )
-			return "Please enter a nine-digit US phone number in the format xxx-xxx-xxxx.";
+	if (!frm.phone.match(/^[( ]*[0-9]{3}[) -]*[0-9]{3}[ -]*[0-9]{4}$/) )
+		return "Please enter a nine-digit US phone number in the format xxx-xxx-xxxx.";
 
-		return "valid";
-	}
+	return "valid";
+
+}
