9ea1500621a9aec288b8acd7a6e543bc7492ab5c
Swolebrain <thecodingteacher@gmail.com>
2017-07-09 21:15:18 -0400

    fix: allowing content type headers

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Header', "Content-Type")
 	next();
 });
 
